10439705dbc076abb2e99645587c4a8896a2c0b9
Sana S Navas <sanasnavas10@gmail.com>
2024-12-30 21:59:39 +0530

    Update 4.stack-linked.c

--- 4.stack-linked.c
+++ 4.stack-linked.c
@@ -1,92 +1,94 @@
-
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
-void push();
-void pop();
-void peek();
-void display();
-
 
 struct node {
     int data;
     struct node *next;
 };
 
-struct node *top = NULL; 
-int main() {
-    int choice, value;
+struct node *top = NULL;
 
+void push();
+void pop();
+void peek();
+void display();
+
+int main() {
     while (1) {
-        printf("\n--- Stack Operations Menu ---\n");
+        int choice;
+        printf("\nEnter your choice:\n");
         printf("1. Push\n");
         printf("2. Pop\n");
         printf("3. Peek\n");
         printf("4. Display\n");
         printf("5. Exit\n");
-        printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1: // Push
-                printf("Enter the value to push: ");
-                scanf("%d", &value);
-                push(value);
-                break;
-            case 2: // Pop
-                pop();
-                break;
-            case 3: // Peek
-                peek();
-                break;
-            case 4: // Display
-                display();
-                break;
-            case 5: // Exit
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid choice! Please try again.\n");
         }
     }
     return 0;
 }
-void push(int x) {
+
+void push() {
+    int value;
+    printf("\nEnter value to insert: ");
+    scanf("%d", &value);
+    
     struct node *newnode = (struct node *)malloc(sizeof(struct node));
-    newnode->data = x;
+    newnode->data = value;
     newnode->next = top;
     top = newnode;
-    printf("%d pushed to stack\n", x);
+    
+    printf("Element %d pushed onto the stack.\n", value);
 }
 
-void display() {
+void pop() {
     struct node *temp = top;
     if (top == NULL) {
-        printf("Stack is empty\n");
-        
+        printf("Stack is empty! Cannot pop.\n");
+    } else {
+        printf("Popped element: %d\n", top->data);
+        top = top->next;
+        free(temp);
     }
-    printf("Stack elements: ");
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
 }
 
 void peek() {
     if (top == NULL) {
-        printf("Stack is empty\n");
+        printf("Stack is empty! No element to peek.\n");
     } else {
-        printf("Top element is %d\n", top->data);
+        printf("Peek element: %d\n", top->data);
     }
 }
 
-void pop() {
-    struct node *temp=top;
-    if (top == NULL) {
-        printf("No elements to pop\n");
+void display() {
+    struct node *temp = top;
+    if (temp == NULL) {
+        printf("Stack is empty! Nothing to display.\n");
+    } else {
+        printf("\nStack elements: ");
+        while (temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
     }
-   
-    printf("Popped element: %d\n", top->data);
-    top = top->next;
-    free(temp);
 }
