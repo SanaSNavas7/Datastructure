4392778131b4c5f6681706988965ee29d764e603
Sana S Navas <sanasnavas10@gmail.com>
2025-01-05 12:22:30 +0530

    Update Topo2.c

--- Topo2.c
+++ Topo2.c
@@ -1,1 +1,50 @@
 
+#include <stdio.h>
+#define MAX 5
+void main()
+{
+    int n;
+    printf("enter no of vertices");
+    scanf("%d", &n);
+    int visited[MAX] = {0}, adj[MAX][MAX] = {0}, i, j, current;
+    int count = 0;
+    int flag = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("adj[%d][%d]", i, j);
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    while (count != n)
+    {
+        for (i = 0; i < n; i++)
+        {
+            flag = 0;
+            if (visited[i] == 0)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (adj[j][i] == 1)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                    if (flag == 0)
+                    {
+                        current = i;
+                        printf("%d", current);
+                        visited[current] = 1;
+                        count++;
+                        for (int i = 0; i < n; i++)
+                        {
+                            adj[current][i] = 0;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
