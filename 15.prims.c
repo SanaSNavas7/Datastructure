1d6957f4bc65f4b451a11fe9f08eeee729552d98
Sana S Navas <sanasnavas10@gmail.com>
2025-01-07 20:36:03 +0530

    Update 15.prims.c

--- 15.prims.c
+++ 15.prims.c
@@ -1,55 +1,62 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+
 void main()
 {
-	int n;
-	printf("Enter no:of vertices: ");
-	scanf("%d",&n);
-	int A[n][n],vi[n],u,v,min=999,cost=0,e=1;
-	printf("Enter adjacency matrix: ");
-	for(int i=0;i<n;i++)
-	{
-		vi[i]=0;
-		for(int j=0;j<n;j++)
-		{
-			scanf("%d",&A[i][j]);
-			if(A[i][j]==0)
-				A[i][j]=999;
-			if(A[i][j]<min)
-			{
-				min=A[i][j];
-				u=i;
-				v=j;
-			}
-		}
-	}
-	cost+=min;
-	vi[u]=1;
-	vi[v]=1;
-	printf("\nSpanning Tree\n");
-	printf("Edge {%d,%d}= %d\n",u,v,min);
-	while(e<n-1) // Minimum spanning tree will always have n-1 edges.
-	{
-		min=999;
-		for(int i=0;i<n;i++) //checks if selected start vertex is visited 
-		{
-			if(vi[i]==1) // if visited then:
-			{
-				for(int j=0;j<n;j++) // loop to find mininmum weight edge connected to start vertex 
-				{
-					if(A[i][j]<min && vi[j]==0)
-					{
-						min=A[i][j];
-						u=i;
-						v=j;
-					}
-				}
-			}
-		}
-		cost+=min;
-		vi[v]=1; // make end vertex visited
-		printf("Edge {%d,%d}= %d\n",u,v,min);
-		e+=1;
-	}
-	printf("Cost = %d",cost);
+    int n;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    
+    int A[n][n], vi[n], i, j, u, v, min, e = 1, cost = 0;
+    
+    // Initialize visited array
+    for (i = 0; i < n; i++)
+    {
+        vi[i] = 0;  // All vertices are initially not included in MST
+    }
+    
+    // Input adjacency matrix
+    printf("Enter adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &A[i][j]);
+            if (A[i][j] == 0 && i != j) // No edge case
+            {
+                A[i][j] = 999; // Use 999 to represent no edge between vertices
+            }
+        }
+    }
+    
+    // Choose the first vertex in the MST (vertex 0)
+    vi[0] = 1;  // Start from vertex 0
+    while (e < n - 1)
+    {
+        min = 999;  // Reset the minimum edge value for the next iteration
+        // Loop to find the minimum edge connecting the MST to a vertex outside it
+        for (i = 0; i < n; i++)
+        {
+            if (vi[i] == 1)  // Check only vertices already in MST
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if (A[i][j] < min && vi[j] == 0)  // Find edge to non-MST vertex
+                    {
+                        min = A[i][j];
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+        }
+
+        // Add the selected edge to the MST
+        cost += min;
+        vi[v] = 1;  // Mark the vertex 'v' as part of the MST
+        e++;
+        printf("Edge {%d, %d}: %d\n", u + 1, v + 1, min);  // Print the edge and its weight
+    }
+    
+    // Print the total cost of the MST
+    printf("Total cost of MST: %d\n", cost);
 }
